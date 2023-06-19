c43f1ac5e684a49afba608bb541d24dcd17c75ad
Vamshi-Puttakota <127819125+Vamshi-Puttakota@users.noreply.github.com>
2023-06-19 13:11:42 +0530

    Delete Orders directory

--- Orders/src/main/java/com/orders/repository/ItemsHistoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.orders.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.orders.entities.ItemsHistory;
-
-public interface ItemsHistoryRepository extends JpaRepository<ItemsHistory, Integer>{
-
-}
