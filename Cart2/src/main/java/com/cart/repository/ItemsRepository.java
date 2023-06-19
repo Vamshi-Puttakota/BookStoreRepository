51dd3de6fd61e3763b4a8ca1f53032ed537697f5
Vamshi-Puttakota <127819125+Vamshi-Puttakota@users.noreply.github.com>
2023-06-19 13:12:21 +0530

    Delete Cart2 directory

--- Cart2/src/main/java/com/cart/repository/ItemsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cart.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cart.entities.Items;
-
-public interface ItemsRepository extends JpaRepository<Items, Integer> {
-
-}
